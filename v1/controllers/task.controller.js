7ad2054799bb3267488b905dc01516aa9e0dfd6f
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-03 20:39:01 +0700

    filter status and sort

--- v1/controllers/task.controller.js
+++ v1/controllers/task.controller.js
@@ -2,9 +2,22 @@ const Task = require("../models/task.model");
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
-  const tasks = await Task.find({
+  const find = {
     deleted: false
-  });
+  };
+
+  if(req.query.status) {
+    find.status = req.query.status;
+  }
+
+  // Sort
+  const sort = {};
+  if(req.query.sortKey && req.query.sortValue){
+    sort[req.query.sortKey] = req.query.sortValue;
+  }
+  // End Sort
+
+  const tasks = await Task.find(find).sort(sort);
 
   res.json(tasks);
 };

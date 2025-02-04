8932cea8cc68fd8972940983453e3931be2077ad
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-04 18:00:23 +0700

    send otp to email

--- v1/controllers/user.controller.js
+++ v1/controllers/user.controller.js
@@ -1,6 +1,9 @@
 const md5 = require("md5");
 const User = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const generateHelper = require("../../helpers/generate.helper");
+const sendEmailHelper = require("../../helpers/sendEmail.helper");
+const ForgotPassword = require("../models/forgot-password.model");
 
 // [POST] /api/v1/users/register
 module.exports.register = async (req, res) => {
@@ -66,5 +69,43 @@ module.exports.login = async (req, res) => {
     code: 200,
     message: "Đăng nhập thành công!",
     token: token
+  });
+}
+
+// [POST] /api/v1/users/password/forgot
+module.exports.passwordForgot = async (req, res) => {
+  const email = req.body.email;
+
+  const existUser = await User.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if(!existUser) {
+    res.json({
+      code: 400,
+      message: "Email không tồn tại!"
+    });
+    return;
+  }
+
+  const otp = generateHelper.generateRandomNumber(6);
+
+  const objectForgotPassword = {
+    email: email,
+    otp: otp,
+    expireAt: Date.now() + 3*60*1000
+  };
+
+  const forgotPassword = new ForgotPassword(objectForgotPassword);
+  await forgotPassword.save();
+
+  const subject = "Lấy lại mật khẩu.";
+  const text = `Mã OTP xác thực tài khoản của bạn là: ${otp}. Mã OTP có hiệu lực trong vòng 3 phút. Vui lòng không cung cấp mã OTP này với bất kỳ ai.`;
+  sendEmailHelper.sendEmail(email, subject, text);
+
+  res.json({
+    code: 200,
+    message: "Đã gửi mã OTP qua email!"
   });
 }
--- v1/routes/user.route.js
+++ v1/routes/user.route.js
@@ -7,4 +7,6 @@ router.post("/register", controller.register);
 
 router.post("/login", controller.login);
 
+router.post("/password/forgot", controller.passwordForgot);
+
 module.exports = router;

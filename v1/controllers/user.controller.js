6ad095dd27ed47a9cd175213736cda7543151270
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-04 16:15:28 +0700

    login user

--- v1/controllers/user.controller.js
+++ v1/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const md5 = require("md5");
-const User = require("../models/user.model")
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 // [POST] /api/v1/users/register
 module.exports.register = async (req, res) => {
@@ -28,5 +29,42 @@ module.exports.register = async (req, res) => {
   res.json({
     code: 200,
     message: "Đăng ký tài khoản thành công!",
+  });
+}
+
+// [POST] /api/v1/users/login
+module.exports.login = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const existUser = await User.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if(!existUser) {
+    res.json({
+      code: 400,
+      message: "Email hoặc mật khẩu không đúng!"
+    });
+    return;
+  }
+  
+  if(md5(password) != existUser.password) {
+    res.json({
+      code: 400,
+      message: "Email hoặc mật khẩu không đúng!"
+    });
+    return;
+  }
+
+  const token = jwt.sign({id: existUser.id}, process.env.JWT_KEY, {
+    expiresIn: "1d"
+  });
+
+  res.json({
+    code: 200,
+    message: "Đăng nhập thành công!",
+    token: token
   });
 }
